e4d170bd277339fd0c103aa501da39705da23ac7
Alexander Senko <Alexander.Senko@gmail.com>
2017-05-22 09:13:13 +0700

    Core: added `available`, `borrowed` & `delayed` book scopes.

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,4 +3,17 @@ class Book < ApplicationRecord
   has_many   :requests
 
   validates_presence_of :title, :author, :released_on, :category
+
+
+  scope :available, -> {
+    where.not id: borrowed # TODO: get rid of the subquery
+  }
+
+  scope :borrowed, -> {
+    joins(:requests).where(requests: { returned_on: nil })
+  }
+
+  scope :delayed, -> {
+    joins(:requests).where('requests.should_return_on < ?', Date.today)
+  }
 end
--- test/models/book_test.rb
+++ test/models/book_test.rb
@@ -1,7 +1,35 @@
 require 'test_helper'
 
 class BookTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'available' do
+    assert_includes     Book.available, books(:hobbit)
+    assert_not_includes Book.available, books(:magic_guide)
+    assert_includes     Book.available, books(:history_of_mordor)
+  end
+
+  test 'borrowed' do
+    assert_not_includes Book.borrowed, books(:hobbit)
+    assert_includes     Book.borrowed, books(:magic_guide)
+    assert_not_includes Book.borrowed, books(:history_of_mordor)
+  end
+
+  test 'delayed' do
+    travel_to Date.new(3017, 7, 1) do
+      assert_not_includes Book.delayed, books(:hobbit)
+      assert_not_includes Book.delayed, books(:magic_guide)
+      assert_not_includes Book.delayed, books(:history_of_mordor)
+    end
+
+    travel_to Date.new(3017, 8, 1) do
+      assert_not_includes Book.delayed, books(:hobbit)
+      assert_not_includes Book.delayed, books(:magic_guide)
+      assert_not_includes Book.delayed, books(:history_of_mordor)
+    end
+
+    travel_to Date.new(3017, 9, 1) do
+      assert_not_includes Book.delayed, books(:hobbit)
+      assert_includes     Book.delayed, books(:magic_guide)
+      assert_not_includes Book.delayed, books(:history_of_mordor)
+    end
+  end
 end

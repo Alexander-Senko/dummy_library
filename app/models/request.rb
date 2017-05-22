25650682a307b8c52c1e746b3ec1b598bb2a49f7
Alexander Senko <Alexander.Senko@gmail.com>
2017-05-22 17:24:35 +0700

    Core: added `active` & `expired` request scopes.

--- app/models/request.rb
+++ app/models/request.rb
@@ -2,4 +2,14 @@ class Request < ApplicationRecord
   belongs_to :book
 
   validates_presence_of :book, :borrower, :borrowed_on, :should_return_on
+
+
+  scope :active, -> {
+    where returned_on: nil
+  }
+
+  scope :expired, -> (date = Date.today) {
+    active
+        .where 'should_return_on < ?', date
+  }
 end
--- test/models/request_test.rb
+++ test/models/request_test.rb
@@ -1,7 +1,30 @@
 require 'test_helper'
 
 class RequestTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'active' do
+    assert_not_includes Request.active, requests(:gandalf)
+    assert_includes     Request.active, requests(:smeagol)
+  end
+
+  test 'expired' do
+    travel_to Date.new(3017, 7, 1) do
+      assert_not_includes Request.expired, requests(:gandalf)
+      assert_not_includes Request.expired, requests(:smeagol)
+    end
+
+    travel_to Date.new(3017, 8, 1) do
+      assert_not_includes Request.expired, requests(:gandalf)
+      assert_not_includes Request.expired, requests(:smeagol)
+    end
+
+    travel_to Date.new(3017, 9, 1) do
+      assert_not_includes Request.expired, requests(:gandalf)
+      assert_includes     Request.expired, requests(:smeagol)
+    end
+
+    travel_to Date.new(3019, 5, 1) do
+      assert_not_includes Request.expired, requests(:gandalf)
+      assert_includes     Request.expired, requests(:smeagol)
+    end
+  end
 end
